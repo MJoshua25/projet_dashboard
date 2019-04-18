1a0523d421bcfbcb243b8c3d7dd22d9294c7fb62
MJoshua25 <jyao2000@yahoo.fr>
2019-04-18 15:36:14 +0000

    avance

--- Backend Node/app.js
+++ Backend Node/app.js
@@ -7,3 +7,8 @@ const mysql = require('promise-mysql')
 const morgan = require('morgan')('dev')
 const axios = require('axios')
 const {success, error, checkAndChange} = require('./assets/functions')
+
+
+const multer = require('multer')
+
+mysql.createConnection
