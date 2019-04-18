651cb6f8049bcfdbbe6280a81ebfc2b33a6211dc
MJoshua25 <jyao2000@yahoo.fr>
2019-04-18 15:16:56 +0000

    Demarrage  du projet node

---
+++ Backend Node/app.js
@@ -0,0 +1,9 @@
+// Modules
+
+require('babel-register')
+const bodyParser = require('body-parser')
+const express = require('express')
+const mysql = require('promise-mysql')
+const morgan = require('morgan')('dev')
+const axios = require('axios')
+const {success, error, checkAndChange} = require('./assets/functions')
