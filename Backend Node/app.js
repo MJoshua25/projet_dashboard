c38bb2f21ee91bb66cf32425667efe7e007f16f5
Josué Yao <jyao2000@yahoo.fr>
2019-05-03 08:28:28 +0000

    avancé dans le projet node

--- Backend Node/app.js
+++ Backend Node/app.js
@@ -6,9 +6,32 @@ const express = require('express')
 const mysql = require('promise-mysql')
 const morgan = require('morgan')('dev')
 const axios = require('axios')
+const config = require('./assets/config')
 const {success, error, checkAndChange} = require('./assets/functions')
 
 
 const multer = require('multer')
 
-mysql.createConnection
+mysql.createConnection(config.db)
+    .then((db) => {
+        const app = express();
+        let Users = require('./models/Users-class')
+        const server = require('http').createServer(app)
+        var io = require('socket.io').listen(server)
+
+        var usersRouter = require('./routes/index')
+
+        app.set('views', path.join(__dirname, 'views'));
+        app.set('view engine', 'twig');
+        app.use(session({secret:"dashboard"}))
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(express.static(path.join(__dirname, 'public')));
+
+        app.use('/users', usersRouter)
+
+        server.listen(config.port, () => {
+            console.log("Started on port " + config.port)
+        })
+    })
+.catch(err => console.log(err.message))
---
+++ Backend Node/routes/index.js
@@ -0,0 +1,9 @@
+let Users;
+
+module.exports = (_Users) => {
+    Users = _Users
+    return router
+};
+
+var express = require('express');
+var router = express.Router();
