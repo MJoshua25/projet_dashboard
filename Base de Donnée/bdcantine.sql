5cd154e0fdb836d6e02209002f1358fd1f7243c5
Josué Yao <jyao2000@yahoo.fr>
2019-05-03 12:21:22 +0000

    Ajout des relations dans la base de donnée

--- Base de Donnée/bdcantine.sql
+++ Base de Donnée/bdcantine.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  jeu. 18 avr. 2019 à 14:41
--- Version du serveur :  10.1.38-MariaDB
--- Version de PHP :  7.3.3
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  ven. 03 mai 2019 à 12:19
+-- Version du serveur :  5.7.23
+-- Version de PHP :  7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -22,21 +22,39 @@ SET time_zone = "+00:00";
 -- Base de données :  `bdcantine`
 --
 
+DELIMITER $$
+--
+-- Procédures
+--
+DROP PROCEDURE IF EXISTS `addSchool`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addSchool` (IN `s_name` VARCHAR(255), IN `s_loc` VARCHAR(255))  NO SQL
+    COMMENT 'fonction d''ajout d''école'
+INSERT INTO schools(name, localisation) VALUES(s_name, s_loc)$$
+
+DROP PROCEDURE IF EXISTS `getAllSchool`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllSchool` ()  NO SQL
+    COMMENT 'Afficher toute les écoles'
+SELECT * from schools ORDER by localisation$$
+
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `admin`
 --
 
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
   `lastname` varchar(200) NOT NULL,
   `email` varchar(200) NOT NULL,
   `phone` varchar(225) NOT NULL,
   `avatar` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `date_reg` datetime NOT NULL
+  `date_reg` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -45,9 +63,11 @@ CREATE TABLE `admin` (
 -- Structure de la table `category`
 --
 
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -56,11 +76,13 @@ CREATE TABLE `category` (
 -- Structure de la table `commands`
 --
 
-CREATE TABLE `commands` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `commands`;
+CREATE TABLE IF NOT EXISTS `commands` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` int(11) NOT NULL,
   `date_okey` datetime NOT NULL,
-  `date_commande` datetime NOT NULL
+  `date_commande` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -69,12 +91,16 @@ CREATE TABLE `commands` (
 -- Structure de la table `comments`
 --
 
-CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `content` varchar(200) NOT NULL,
-  `date_comment` datetime NOT NULL
+  `date_comment` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comments_items_id_fk` (`item_id`),
+  KEY `comments_users_id_fk` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -83,11 +109,15 @@ CREATE TABLE `comments` (
 -- Structure de la table `c_content`
 --
 
-CREATE TABLE `c_content` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `c_content`;
+CREATE TABLE IF NOT EXISTS `c_content` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_item` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `id_commande` int(11) NOT NULL
+  `id_commande` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `c_content_commands_id_fk` (`id_commande`),
+  KEY `c_content_items_id_fk` (`id_item`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -96,14 +126,17 @@ CREATE TABLE `c_content` (
 -- Structure de la table `items`
 --
 
-CREATE TABLE `items` (
-  `id` int(11) NOT NULL,
-  `categoey_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE IF NOT EXISTS `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `price` decimal(10,0) NOT NULL,
   `description` varchar(150) NOT NULL,
   `image` varchar(100) NOT NULL,
-  `date_reg` datetime NOT NULL
+  `date_reg` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `items_category_id_fk` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -112,11 +145,15 @@ CREATE TABLE `items` (
 -- Structure de la table `likes`
 --
 
-CREATE TABLE `likes` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE IF NOT EXISTS `likes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `date_like` datetime NOT NULL
+  `date_like` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `likes_items_id_fk` (`item_id`),
+  KEY `likes_users_id_fk` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -125,11 +162,14 @@ CREATE TABLE `likes` (
 -- Structure de la table `menu`
 --
 
-CREATE TABLE `menu` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE IF NOT EXISTS `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `week_day` int(11) NOT NULL,
   `item_id` int(11) NOT NULL,
-  `date_add` datetime NOT NULL
+  `date_add` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `menu_items_id_fk` (`item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -138,12 +178,14 @@ CREATE TABLE `menu` (
 -- Structure de la table `m_comments`
 --
 
-CREATE TABLE `m_comments` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `m_comments`;
+CREATE TABLE IF NOT EXISTS `m_comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `week_day` int(11) NOT NULL,
   `content` varchar(200) NOT NULL,
-  `date_comment` datetime NOT NULL
+  `date_comment` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -152,11 +194,20 @@ CREATE TABLE `m_comments` (
 -- Structure de la table `schools`
 --
 
-CREATE TABLE `schools` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `schools`;
+CREATE TABLE IF NOT EXISTS `schools` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `localisation` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `localisation` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `schools`
+--
+
+INSERT INTO `schools` (`id`, `name`, `localisation`) VALUES
+(2, 'LCA', 'Abidjan');
 
 -- --------------------------------------------------------
 
@@ -164,8 +215,9 @@ CREATE TABLE `schools` (
 -- Structure de la table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `lastname` varchar(255) NOT NULL,
@@ -174,148 +226,53 @@ CREATE TABLE `users` (
   `school_id` int(11) NOT NULL,
   `password` varchar(255) NOT NULL,
   `avatar` varchar(100) NOT NULL,
-  `date_reg` datetime NOT NULL
+  `date_reg` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_schools_id_fk` (`school_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Index pour les tables déchargées
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Index pour la table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `commands`
---
-ALTER TABLE `commands`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `comments`
+-- Contraintes pour la table `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
+  ADD CONSTRAINT `comments_items_id_fk` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`),
+  ADD CONSTRAINT `comments_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
--- Index pour la table `c_content`
+-- Contraintes pour la table `c_content`
 --
 ALTER TABLE `c_content`
-  ADD PRIMARY KEY (`id`);
+  ADD CONSTRAINT `c_content_commands_id_fk` FOREIGN KEY (`id_commande`) REFERENCES `commands` (`id`),
+  ADD CONSTRAINT `c_content_items_id_fk` FOREIGN KEY (`id_item`) REFERENCES `items` (`id`);
 
 --
--- Index pour la table `items`
+-- Contraintes pour la table `items`
 --
 ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
+  ADD CONSTRAINT `items_category_id_fk` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 
 --
--- Index pour la table `likes`
+-- Contraintes pour la table `likes`
 --
 ALTER TABLE `likes`
-  ADD PRIMARY KEY (`id`);
+  ADD CONSTRAINT `likes_items_id_fk` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`),
+  ADD CONSTRAINT `likes_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
--- Index pour la table `menu`
+-- Contraintes pour la table `menu`
 --
 ALTER TABLE `menu`
-  ADD PRIMARY KEY (`id`);
+  ADD CONSTRAINT `menu_items_id_fk` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`);
 
 --
--- Index pour la table `m_comments`
---
-ALTER TABLE `m_comments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `schools`
---
-ALTER TABLE `schools`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `users`
+-- Contraintes pour la table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `commands`
---
-ALTER TABLE `commands`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `c_content`
---
-ALTER TABLE `c_content`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `items`
---
-ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `likes`
---
-ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `menu`
---
-ALTER TABLE `menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `m_comments`
---
-ALTER TABLE `m_comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `schools`
---
-ALTER TABLE `schools`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  ADD CONSTRAINT `users_schools_id_fk` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
